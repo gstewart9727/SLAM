44c1734ac13d992f724ec8e60103802a2d9b6f6f
Gabriel <gstewart9727@conestogac.on.ca>
2021-03-05 14:12:21 -0500

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/gabriel/MyWorkspace/src
+# Build directory: /home/gabriel/MyWorkspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("slam")
+subdirs("orb_slam_2_ros")
---
+++ build/orb_slam_2_ros/CMakeFiles/DBoW2.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/orb_slam2/Thirdparty/DBoW2/DBoW2/BowVector.cpp" "/home/gabriel/MyWorkspace/build/orb_slam_2_ros/CMakeFiles/DBoW2.dir/orb_slam2/Thirdparty/DBoW2/DBoW2/BowVector.cpp.o"
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/orb_slam2/Thirdparty/DBoW2/DBoW2/FORB.cpp" "/home/gabriel/MyWorkspace/build/orb_slam_2_ros/CMakeFiles/DBoW2.dir/orb_slam2/Thirdparty/DBoW2/DBoW2/FORB.cpp.o"
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/orb_slam2/Thirdparty/DBoW2/DBoW2/FeatureVector.cpp" "/home/gabriel/MyWorkspace/build/orb_slam_2_ros/CMakeFiles/DBoW2.dir/orb_slam2/Thirdparty/DBoW2/DBoW2/FeatureVector.cpp.o"
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/orb_slam2/Thirdparty/DBoW2/DBoW2/ScoringObject.cpp" "/home/gabriel/MyWorkspace/build/orb_slam_2_ros/CMakeFiles/DBoW2.dir/orb_slam2/Thirdparty/DBoW2/DBoW2/ScoringObject.cpp.o"
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/orb_slam2/Thirdparty/DBoW2/DUtils/Random.cpp" "/home/gabriel/MyWorkspace/build/orb_slam_2_ros/CMakeFiles/DBoW2.dir/orb_slam2/Thirdparty/DBoW2/DUtils/Random.cpp.o"
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/orb_slam2/Thirdparty/DBoW2/DUtils/Timestamp.cpp" "/home/gabriel/MyWorkspace/build/orb_slam_2_ros/CMakeFiles/DBoW2.dir/orb_slam2/Thirdparty/DBoW2/DUtils/Timestamp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"orb_slam2_ros\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/gabriel/MyWorkspace/devel/include"
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/orb_slam2"
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/orb_slam2/include"
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/ros/include"
+  "/usr/include/eigen3"
+  "/opt/ros/melodic/include"
+  "/opt/ros/melodic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/orb_slam2/Thirdparty/g2o/g2o/core"
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/orb_slam2/Thirdparty/g2o/g2o/types"
+  "/home/gabriel/MyWorkspace/src/orb_slam_2_ros/orb_slam2/Thirdparty/g2o/g2o/stuff"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
